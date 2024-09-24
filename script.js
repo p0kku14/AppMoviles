60174ce3c559763694b6d8bc75bb9e416972d695
p0kku14 <velazquez.molina.valeria@gmail.com>
2024-09-23 20:49:08 -0600

    comentario de los cambios

---
+++ script.js
@@ -0,0 +1,32 @@
+//18/09/24 / Velazquez Molina Valeria
+//comentario en una sola línea
+/*Comentario
+en mutilineas */
+
+
+//Variables
+/*
+Las variables guardan información reutilizable
+*/
+
+
+//Declaración de las variables
+let nombre = "valeria"
+const PI = 3.1416;
+var edad = 16;
+
+
+/*Tipos de datos
+Cadena de texto(String): "Hola",'mundo'
+Número(number): 5,13,14
+Booleanos (boolean):(true , false)
+*/
+
+
+//Ejemplo
+let mensaje = 'Hola Mundo'
+let numero = 42;
+let Estudiante = true;
+
+
+//Operadores matemáticos
