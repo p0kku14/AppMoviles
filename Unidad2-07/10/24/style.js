41ed2d992a4eeea880e782d39bbd10d10fed19b1
p0kku14 <velazquez.molina.valeria@gmail.com>
2024-10-07 20:34:08 -0600

    Clase 07/10/24

---
+++ Unidad2-07/10/24/style.js
@@ -0,0 +1,19 @@
+function appendToDisplay(value){
+    document.getElementById('display').value += value;
+}
+function clearDisplay(){
+    document.getElementById('display').value ='';
+}
+function deleteLast(){
+    document.getElementById('display').value = displayValue.slice(0,-1);
+}
+
+function calculateResult(){
+    try{
+        let result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error){
+        alert('Expresión inválida');
+    }
+    
+}
